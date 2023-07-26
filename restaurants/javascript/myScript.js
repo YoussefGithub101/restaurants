39022fc659345dc92e29b92ef95006b7aae3e8d5
YoussefGitHub101 <yousseffredy10@gmail.com>
2023-07-26 03:48:05 +0300

    restaurants

---
+++ restaurants/javascript/myScript.js
@@ -0,0 +1,8 @@
+document.addEventListener('scroll',()=>{
+    const header =document.querySelector('nav')
+    if(window.scrollY > 0){
+      header.classList.add('scrolled')
+    }else{
+      header.classList.remove('scrolled')
+    }
+  })
